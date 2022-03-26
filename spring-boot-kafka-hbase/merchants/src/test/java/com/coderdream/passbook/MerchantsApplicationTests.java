ce8fe09d82cc82a01eb9e6d4c19cfa3e1b5cb5cf
CoderDream <coderdream@gmail.com>
2022-03-26 13:03:53 +0800

    卡包初始化版本

---
+++ spring-boot-kafka-hbase/merchants/src/test/java/com/coderdream/passbook/MerchantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderdream.passbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
