56c9d860a72e125c70b34b1421ac2d51d9b53d6f
CoderDream <coderdream@gmail.com>
2022-04-17 10:59:47 +0800

    ch02_搭建SpringBoot微服务项目

---
+++ middleware/practice/ch02/middleware/server/src/main/java/com/coderdream/middleware/server/MainApplication.java
@@ -0,0 +1,24 @@
+package com.coderdream.middleware.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * 应用启动类-入口
+ * @Author:
+ **/
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+
+}
