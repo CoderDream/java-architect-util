d9141801a8667ee475cfa25af9bc22fc87499abb
CoderDream <coderdream@gmail.com>
2022-04-23 19:48:47 +0800

    5.1　RabbitMQ概述

--- middleware/practice/ch05/middleware/server/src/main/java/com/coderdream/middleware/server/event/Publisher.java
+++ middleware/practice/ch05/middleware/server/src/main/java/com/coderdream/middleware/server/event/Publisher.java
@@ -14,13 +14,24 @@ import java.util.Date;
  */
 @Component
 public class Publisher {
-
+    /**
+     * 定义日志
+     */
     private static final Logger log = LoggerFactory.getLogger(Publisher.class);
 
+    /**
+     * 定义发送消息的组件
+     */
     @Resource
     private ApplicationEventPublisher applicationEventPublisher;
 
+    /**
+     * 发送消息的方法
+     *
+     * @throws Exception
+     */
     public void sendMsg() throws Exception {
+        // 构造登陆
         LoginEvent event = new LoginEvent(this, "debug", new SimpleDateFormat("yyyyy-MM-dd HH:mm:ss").format(new Date()), "127.0.0.1");
         applicationEventPublisher.publishEvent(event);
         log.info("Spring事件驱动模型-发送消息：{}", event);
