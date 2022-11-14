b9c29ef333f937c8ab22ae36ac8104c49b8cb7e0
CoderDream <coderdream@gmail.com>
2022-11-14 10:23:54 +0800

    Excel校验

---
+++ easyexcel-import-validate/README.md
@@ -0,0 +1,21 @@
+# easyexcel-import-validate
+根据阿里的easyexcel通过validation+正则实现excel导入校验
+包含了简单的导入导入导出demo，所涉及到的都是web的导入导出
+
+### Postman 调用接口
+
+#### 正常
+
+ ![image-20221114101632070](assets\image-20221114101632070.png)
+
+#### 有出错记录
+
+* 请求时选【Send and Download】
+
+ ![image-20221114101757687](assets\image-20221114101757687.png)
+
+ ![image-20221114101913083](assets\image-20221114101913083.png)
+
+#### 记录详情
+
+![image-20221114102015335](assets\image-20221114102015335.png)
---
+++ easyexcel-import-validate/src/main/java/com/zhy/easyexceldemo/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.zhy.easyexceldemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket getUserDocket(){
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        List<Parameter> parameters = new ArrayList<>();
+        ApiInfo apiInfo=new ApiInfoBuilder()
+                .title("数据中心平台")//api标题
+                .description("数据中心相关接口描述")//api描述
+                .version("1.0.0")//版本号
+                .contact(new Contact("XX科技",null,null))
+                .build();
+        return new Docket(DocumentationType.SWAGGER_2)//文档类型（swagger2）
+                .apiInfo(apiInfo)//设置包含在json ResourceListing响应中的api元信息
+                .select()//启动用于api选择的构建器
+                .apis(RequestHandlerSelectors.basePackage("com.zhy.easyexceldemo.rest"))//扫描接口的包
+                .paths(PathSelectors.any())//路径过滤器（扫描所有路径）
+                .build().globalOperationParameters(parameters);
+    }
+
+}
