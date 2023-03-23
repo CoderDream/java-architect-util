56a26450228d039f58ae7b91aa4ec4989ec3ecd4
CoderDream <coderdream@gmail.com>
2023-03-23 14:05:25 +0800

    自动生成代码

---
+++ mybatis-plus-mybatisx/src/main/java/com/coderdream/freeapps/service/ResourceConfigService.java
@@ -0,0 +1,13 @@
+package com.coderdream.freeapps.service;
+
+import com.coderdream.freeapps.model.ResourceConfigEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author CoderDream
+* @description 针对表【t_resource_config】的数据库操作Service
+* @createDate 2023-03-21 14:36:06
+*/
+public interface ResourceConfigService extends IService<ResourceConfigEntity> {
+
+}
---
+++ mybatis-plus-mybatisx/src/main/java/com/coderdream/freeapps/service/impl/ResourceConfigServiceImpl.java
@@ -0,0 +1,22 @@
+package com.coderdream.freeapps.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.coderdream.freeapps.model.ResourceConfigEntity;
+import com.coderdream.freeapps.service.ResourceConfigService;
+import com.coderdream.freeapps.mapper.ResourceConfigMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author CoderDream
+* @description 针对表【t_resource_config】的数据库操作Service实现
+* @createDate 2023-03-21 14:36:06
+*/
+@Service
+public class ResourceConfigServiceImpl extends ServiceImpl<ResourceConfigMapper, ResourceConfigEntity>
+    implements ResourceConfigService{
+
+}
+
+
+
+
